358689c0a3ff6525ecd6794a19949c7fb1314776
cricci3 <claudioricci.99@gmail.com>
2024-07-02 17:02:34 +0200

    rename n

--- DCT_lib.py
+++ DCT_lib.py
@@ -3,25 +3,25 @@ from scipy.fft import dct, idct, dctn
 
 
 def dct_rows_only(input_matrix):
-    N = input_matrix.shape[1]  # Numero di colonne
+    n = input_matrix.shape[1]  # Numero di colonne
 
-    j = np.arange(N)  # Indici fino a N
+    j = np.arange(n)  # Indici fino a N
     i = j[:, None]  # Indici fino a N-1
 
     # Calcola gli argomenti del coseno per ciascuna coppia di indici (i, j).
-    cos_matrix = np.cos(np.pi * (2 * i + 1) * j / (2 * N))
+    cos_matrix = np.cos(np.pi * (2 * i + 1) * j / (2 * n))
 
     # Matrice per il risultato della DCT riga per riga
     output_matrix = np.zeros_like(input_matrix, dtype=float)
 
     # Applica la DCT alle righe
     for x in range(input_matrix.shape[0]):  # Numero di righe
-        for u in range(N):
+        for u in range(n):
             output_matrix[x, u] = np.sum(input_matrix[x] * cos_matrix[:, u])
 
     # Fattore di normalizzazione
-    W = np.sqrt(2 / N) * np.ones(N)
-    W[0] = np.sqrt(1 / N)
+    W = np.sqrt(2 / n) * np.ones(n)
+    W[0] = np.sqrt(1 / n)
 
     # Applica i fattori di normalizzazione
     output_matrix *= W
