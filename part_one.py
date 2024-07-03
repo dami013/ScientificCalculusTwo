d1a2f80e2770d68b07dc7e1aa7a1b15b46caebff
cricci3 <claudioricci.99@gmail.com>
2024-07-03 11:06:45 +0200

    changed lib for times

--- part_one.py
+++ part_one.py
@@ -1,4 +1,3 @@
-import time
 import matplotlib.pyplot as plt
 from DCT_lib import personal_dct2, dct2_library
 import numpy as np
@@ -7,7 +6,7 @@ import timeit
 
 if __name__ == '__main__':
     # Dimensioni delle matrici NxN (da 200 a 1000 con passo 50)
-    matrix_dimensions = list(range(50, 500, 50))
+    matrix_dimensions = list(range(50, 1000, 50))
 
     times_homemade = []
     times_scipy = []
@@ -18,25 +17,13 @@ if __name__ == '__main__':
         np.random.seed(5)
         matrix = np.random.uniform(low=0.0, high=255.0, size=(n, n))
 
-        # start_time = time.time()
-        #personal_dct2(matrix)
-        #total_time = time.time() - start_time
-        #times_homemade.append(total_time)
-        #print("Homemade "+str(n)+" tempo: "+str(total_time))
-
         # Misura il tempo per personal_dct2
-        time_homemade = timeit.timeit(lambda: personal_dct2(matrix), number=10) / 10
+        time_homemade = timeit.timeit(lambda: personal_dct2(matrix), number=3) / 3
         times_homemade.append(time_homemade)
         print(f"Homemade {n} tempo: {time_homemade}")
 
-        # start_time2 = time.time()
-        # dct2_library(matrix)
-        # total_time2 = time.time() - start_time2
-        # times_scipy.append(total_time2)
-        # print("Scipy "+str(n)+" tempo: "+str(total_time2))
-
         # Misura il tempo per dct2_library
-        time_scipy = timeit.timeit(lambda: dct2_library(matrix), number=10) / 10
+        time_scipy = timeit.timeit(lambda: dct2_library(matrix), number=3) / 3
         times_scipy.append(time_scipy)
         print(f"Scipy {n} tempo: {time_scipy}")
 
@@ -45,10 +32,10 @@ if __name__ == '__main__':
     n2_logn = [n ** 2 * np.log(n) / 1e8 for n in matrix_dimensions]
 
     plt.figure(figsize=(10, 6))
-    plt.semilogy(matrix_dimensions, times_scipy, label='Scipy DCT2', color="#00FFFF")
-    plt.semilogy(matrix_dimensions, n2_logn, label='n^2 * log(n)', color="#00FFFF", linestyle='dashed')
-    plt.semilogy(matrix_dimensions, times_homemade, label='DCT2 homemade', color="#00CC00")
-    plt.semilogy(matrix_dimensions, n3, label='n^3', color="#00CC00", linestyle='dashed')
+    plt.semilogy(matrix_dimensions, times_scipy, label='Scipy DCT2', color="#CC0000")
+    plt.semilogy(matrix_dimensions, n2_logn, label='n^2 * log(n)', color="#CC0000", linestyle='dashed')
+    plt.semilogy(matrix_dimensions, times_homemade, label='DCT2 homemade', color="#0080FF")
+    plt.semilogy(matrix_dimensions, n3, label='n^3', color="#0080FF", linestyle='dashed')
 
     plt.xlabel('Dimensione N')
     plt.ylabel('Tempo di esecuzione in secondi')
